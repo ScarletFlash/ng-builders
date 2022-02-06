8b886835f764a5d370420a8fe2f69381de9d017c
ScarletFlash <scarletflash.dev@gmail.com>
2022-02-06 15:08:32 +0300

    build: add package.json generator

--- do-build.ts
+++ do-build.ts
@@ -1,9 +1,16 @@
+import type { PackageJson } from '@npm/types';
 import { build } from 'esbuild';
-import { writeFile } from 'fs/promises';
+import { readFile, writeFile } from 'fs/promises';
 import * as TypeScriptJsonSchema from 'typescript-json-schema';
 import type { BuilderSources } from './build-scripts/builder-sources.interface';
 import { getBuilderSources } from './build-scripts/get-builder-sources.script';
 import { getDirectories } from './build-scripts/get-directories.script';
+
+type PackageJsonKey = keyof PackageJson | string;
+
+const descriptionByBuilderName: Map<string, string> = new Map<string, string>([
+  ['circular-dependencies-linter', 'Linter for circular dependencies'],
+]);
 
 getDirectories(`${__dirname}/packages`)
   .then((builderSourceDirectories: string[]) => {
@@ -11,20 +18,21 @@ getDirectories(`${__dirname}/packages`)
       builderSourceDirectories.map(async (sourceDirectory: string) => await getBuilderSources(sourceDirectory))
     );
   })
-  .then((builderSources: BuilderSources[]) => {
-    builderSources.forEach(async ({ entryPointPath, name, optionsPath }: BuilderSources) => {
+  .then(async (builderSources: BuilderSources[]) => {
+    builderSources.forEach(async (sources: BuilderSources) => {
+      const { entryPointPath, name }: BuilderSources = sources;
       const outDirPath: string = `${__dirname}/dist/${name}`;
 
       await build({
         chunkNames: 'chunks/[name]-[hash]',
         bundle: true,
-        splitting: true,
+        splitting: false,
         external: ['@angular-devkit/architect', '@angular-devkit/core', 'pnpapi', 'rxjs'],
         minify: false,
         platform: 'node',
         sourcemap: 'external',
-        target: 'esnext',
-        format: 'esm',
+        target: 'node16',
+        format: 'cjs',
         treeShaking: true,
         tsconfig: './tsconfig.json',
         mainFields: ['module', 'main', 'browser'],
@@ -35,16 +43,59 @@ getDirectories(`${__dirname}/packages`)
         entryPoints: [entryPointPath],
       });
 
-      const program: TypeScriptJsonSchema.Program = TypeScriptJsonSchema.getProgramFromFiles([optionsPath]);
-      const schemaDefinition: TypeScriptJsonSchema.Definition | null = TypeScriptJsonSchema.generateSchema(
-        program,
-        'BuilderOptions'
-      );
-
-      if (schemaDefinition === null) {
-        throw new Error(`Builder schema creation for "${name}" failed.`);
-      }
-
-      await writeFile(`${outDirPath}/schema.json`, JSON.stringify(schemaDefinition));
+      await generateBuilderSchema(sources, outDirPath);
     });
+
+    await generateBuildersJson(builderSources);
+
+    await generatePackageJson();
   });
+
+async function generateBuilderSchema({ name, optionsPath }: BuilderSources, outDirPath: string): Promise<void> {
+  const program: TypeScriptJsonSchema.Program = TypeScriptJsonSchema.getProgramFromFiles([optionsPath]);
+  const builderSchema: TypeScriptJsonSchema.Definition | null = TypeScriptJsonSchema.generateSchema(
+    program,
+    'BuilderOptions'
+  );
+
+  if (builderSchema === null) {
+    throw new Error(`Builder schema creation for "${name}" failed.`);
+  }
+
+  await writeFile(`${outDirPath}/schema.json`, JSON.stringify(builderSchema));
+}
+
+async function generateBuildersJson(builderSources: BuilderSources[]): Promise<void> {
+  const buildersJsonContent: object = builderSources.reduce((accumulatedData: object, { name }: BuilderSources) => {
+    return {
+      ...accumulatedData,
+      [name]: {
+        description: descriptionByBuilderName.get(name),
+        schema: `./${name}/schema.json`,
+        implementation: `./${name}/builder.js`,
+      },
+    };
+  }, {});
+
+  await writeFile(`${__dirname}/dist/builders.json`, JSON.stringify({ builders: buildersJsonContent }));
+}
+
+async function generatePackageJson(): Promise<void> {
+  const rootPackageJsonData: Record<PackageJsonKey, unknown> = await readFile(
+    `${__dirname}/package.json`,
+    'utf-8'
+  ).then((fileData: string) => JSON.parse(fileData));
+  const allowedFields: Set<PackageJsonKey> = new Set<PackageJsonKey>(['dependencies', 'name', 'version']);
+
+  const sanitizedPackageJsonEntries = Object.entries(rootPackageJsonData).filter(([key, value]: [string, unknown]) => {
+    const stringifiedValue: string = JSON.stringify(value);
+    const valueIsEmpty: boolean = stringifiedValue === '' || stringifiedValue === '[]' || stringifiedValue === '{}';
+    return allowedFields.has(key) && !valueIsEmpty;
+  });
+
+  const resultPackageJsonContent: Record<PackageJsonKey, unknown> = Object.fromEntries([
+    ...sanitizedPackageJsonEntries,
+    ['builders', './builders.json'],
+  ]);
+  await writeFile(`${__dirname}/dist/package.json`, JSON.stringify(resultPackageJsonContent));
+}
